1904ce574c40a8786d691026d924ad7a8fb7c5f9
lnvlna <nikolyalasinskiy@gmail.com>
2025-01-27 17:08:21 +0200

    rtw_proc: Re-enable ResumeTxBeacon() in management frame injection
    - Uncomment ResumeTxBeacon() function call in proc_set_mgnt_inject()
    - Restore beacon transmission mechanism
    - Maintain rtw_mi_tx_beacon_hdl() for beacon transmission

--- os_dep/linux/rtw_proc.c
+++ os_dep/linux/rtw_proc.c
@@ -6337,7 +6337,7 @@ static ssize_t proc_set_mgnt_inject(
     struct net_device *ndev = data;
     _adapter *adapter = (_adapter *)rtw_netdev_priv(ndev);
 
-	//ResumeTxBeacon(adapter);
+	ResumeTxBeacon(adapter);
     rtw_mi_tx_beacon_hdl(adapter);
     //tx_beacon_hdl(adapter, NULL);
 
