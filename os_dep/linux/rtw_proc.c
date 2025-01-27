b5b16cf856e9fd718b06a25d7d76bd6f9d3f61cb
lnvlna <nikolyalasinskiy@gmail.com>
2025-01-27 11:44:01 +0200

    фикс ошибки в rtl8822e

--- os_dep/linux/rtw_proc.c
+++ os_dep/linux/rtw_proc.c
@@ -21,8 +21,6 @@
 #include "../../hal/hal_halmac.h"
 #include "../../hal/rtl8822e/rtl8822e.h"
 #include "../../hal/rtl8822e_hal.h"
-#include "../../hal/rtl8822e/rtl8822e.h"
-#include "../../hal/rtl8822e/rtl8822e_hal.h"
 
 // Если определения отсутствуют в заголовочных файлах, добавьте их:
 #ifndef REG_BCN_INTERVAL_8812E
